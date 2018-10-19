9064cebdd310e337d0ebfa79e61bbad0a0495d69
Slava Vasilev <vcslav@MacBook-Pro-Slava.local>
2018-10-19 19:21:41 +0300

    tst

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,6 @@ def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
-
 bot.remove_webhook()
 bot.set_webhook(url=environ['app_url']+environ['token'])
 app.run(host="0.0.0.0", port=environ.get('PORT', 5000))
--- app/core_bot.py
+++ app/core_bot.py
@@ -1,15 +1,13 @@
 import tele_bot_tools as tbt
 from app import bot
 from os import environ
-
-def main_check(bot):
     
-    @bot.message_handler(commands=['start'])
-    def hi_msg(message):
-        bot.send_message(environ['channel_name'], 'test')
-        bot.send_message(message.chat.id, 'Start', reply_markup=tbt.order_row_keyboard(['a', 'b', 'c', 'd']))
+@bot.message_handler(commands=['start'])
+def hi_msg(message):
+    bot.send_message(environ['channel_name'], 'test')
+    bot.send_message(message.chat.id, 'Start', reply_markup=tbt.order_row_keyboard(['a', 'b', 'c', 'd']))
 
-    @bot.callback_query_handler(func=lambda call: True)
-    def callback_inline(call):
-        if call.data:
-            pass
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.data:
+        pass
