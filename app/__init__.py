263c9a8cf176c9666b51f11614e76468a91bb771
Slava Vasilev <vcslav@MacBook-Pro-Slava.local>
2018-10-19 18:02:23 +0300

    test

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,15 @@
 from os import environ
 from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
+'from flask_sqlalchemy import SQLAlchemy
 import telebot
 
 bot = telebot.TeleBot(environ['token'])
-print ('hi')
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ['DATABASE_URL']
-db = SQLAlchemy(app)
 
-from app import core_bot, models
+app = Flask(__name__)
+'app.config['SQLALCHEMY_DATABASE_URI'] = environ['DATABASE_URL']
+'db = SQLAlchemy(app)
+
+from app import core_bot', models
 
 @app.route("/"+environ['token'], methods=['POST'])
 def getMessage():
