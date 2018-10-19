87a7087fa9d39061d1831925d62f52e823b4570c
Slava Vasilev <vcslav@MacBook-Pro-Slava.local>
2018-10-19 16:02:29 +0300

    test posting in channel

--- core_bot.py
+++ core_bot.py
@@ -1,8 +1,11 @@
 import tele_bot_tools as tbt
+from os import environ
 
 def main_check(bot):
+    
     @bot.message_handler(commands=['start'])
     def hi_msg(message):
+        bot.send_message(environ('channel_name'), 'test')
         bot.send_message(message.chat.id, 'Start', reply_markup=tbt.order_row_keyboard(['a', 'b', 'c', 'd']))
 
     @bot.callback_query_handler(func=lambda call: True)
