6bd23e57fa7f01405782e20983bfe1d92c260695
nuswen <donn.nus@gmail.com>
2018-10-20 14:58:42 +0300

    tst db

--- app/core_bot.py
+++ app/core_bot.py
@@ -1,10 +1,21 @@
 import tele_bot_tools as tbt
 from os import environ
 from app import bot
+from app import db
+from app import models
+
+
+#Рандомно выбираем куда пойдет рынок и выдаем нужную фразу
+def market_wizard():
+    #TODO Выбрать направление
+    #TODO Посмотреть прошлое направление
+    #TODO Выбрать соответствующую фразу из БД и отдать
+    return models.answ.query('Buy')
+
 
 @bot.message_handler(commands=['start'])
 def hi_msg(message):
-    bot.send_message(environ['channel_name'], 'test')
+    bot.send_message(environ['channel_name'], market_wizard())
     bot.send_message(message.chat.id, 'Start', reply_markup=tbt.order_row_keyboard(['a', 'b', 'c', 'd']))
 
 @bot.callback_query_handler(func=lambda call: True)
