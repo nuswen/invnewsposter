eff972dfe9a04b5f3b453d9b562ba03c083c1377
nuswen <donn.nus@gmail.com>
2018-10-20 20:39:32 +0300

    db

--- app/core_bot.py
+++ app/core_bot.py
@@ -48,7 +48,7 @@ tempdb = models.answ.query.get(0)
 lasttime = tempdb.Lasttime
 curtime = int(time.time())
 divtime = curtime - int(lasttime)
-print ('ok')
+print (divtime)
 if divtime > 1800:
         if random.randrange(int(divtime/100),100) > 80:
                 tempdb.Lasttime = curtime
